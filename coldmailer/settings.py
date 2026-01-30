39d98e536c5a5ab782988b1cbfc7d7cf49f63313
Arjun Tomar <arjun16tomar@gmail.com>
2026-01-30 11:08:21 +0530

    Remove django_celery_results from INSTALLED_APPS and update CELERY_RESULT_BACKEND configuration

--- coldmailer/settings.py
+++ coldmailer/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mailer',
-    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -138,7 +137,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379/0")
-CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
